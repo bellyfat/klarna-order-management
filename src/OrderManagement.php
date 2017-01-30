ecf2d54d3dd1a9bb5a8e6a6034b4f6f1a1b81d3a
Jason Grim <jason.grim@klarna.com>
2017-01-30 14:51:04 -0500

    Created base simple integration library for all request to Klarna with auth

---
+++ src/OrderManagement.php
@@ -0,0 +1,221 @@
+<?php namespace KlarnaOrderManagement;
+
+class OrderManagement extends \KlarnaCore\Resource
+{
+    /**
+     * @param string $id
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-get-order
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function get($id)
+    {
+        return $this->client->get(sprintf('ordermanagement/v1/orders/%s', $id));
+    }
+
+    /**
+     * @param string $id
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-acknowledge-order
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function acknowledge($id)
+    {
+        return $this->client->post(sprintf('ordermanagement/v1/orders/%s/acknowledge', $id));
+    }
+
+    /**
+     * @param string $id
+     * @param array  $data
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-set-new-order-amount-and-order-lines
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function change($id, array $data)
+    {
+        return $this->client->post(sprintf('ordermanagement/v1/orders/%s/authorization', $id), [
+            'json' => $data
+        ]);
+    }
+
+    /**
+     * @param string $id
+     * @param array  $data
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-adjust-order-amount-and-order-lines
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function update($id, array $data)
+    {
+        return $this->client->post(sprintf('ordermanagement/v1/orders/%s/authorization-adjustments', $id), [
+            'json' => $data
+        ]);
+    }
+
+    /**
+     * @param string $id
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-cancel-order
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function cancel($id)
+    {
+        return $this->client->post(sprintf('ordermanagement/v1/orders/%s/cancel', $id));
+    }
+
+    /**
+     * @param string $id
+     * @param array  $data
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-update-customer-addresses
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function updateCustomerDetails($id, array $data)
+    {
+        return $this->client->patch(sprintf('ordermanagement/v1/orders/%s/customer-details', $id), [
+            'json' => $data
+        ]);
+    }
+
+    /**
+     * @param string $id
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-extend-authorization-time
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function extendAuthorization($id)
+    {
+        return $this->client->post(sprintf('ordermanagement/v1/orders/%s/cancel', $id));
+    }
+
+    /**
+     * @param string $id
+     * @param array  $data
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-update-merchant-references
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function updateMerchantReferences($id, array $data)
+    {
+        return $this->client->patch(sprintf('ordermanagement/v1/orders/%s/merchant-references', $id), [
+            'json' => $data
+        ]);
+    }
+
+    /**
+     * @param string $id
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-release-remaining-authorization
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function releaseRemainingAuthorization($id)
+    {
+        return $this->client->post(sprintf('ordermanagement/v1/orders/%s/release-remaining-authorization', $id));
+    }
+
+    /**
+     * @param string $id
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-get-all-captures-for-one-order
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getCaptures($id)
+    {
+        return $this->client->get(sprintf('ordermanagement/v1/orders/%s/captures', $id));
+    }
+
+    /**
+     * @param string $id
+     * @param array  $data
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-create-capture
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function capture($id, array $data)
+    {
+        return $this->client->post(sprintf('ordermanagement/v1/orders/%s/captures', $id), [
+            'json' => $data
+        ]);
+    }
+
+    /**
+     * @param string $id
+     * @param string $captureId
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-get-one-capture
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getCapture($id, $captureId)
+    {
+        return $this->client->get(sprintf('ordermanagement/v1/orders/%s/captures/%s', $id, $captureId));
+    }
+
+    /**
+     * @param string $id
+     * @param string $captureId
+     * @param array  $data
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-add-shipping-info-to-a-capture
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function addShippingDetailsToCapture($id, $captureId, array $data)
+    {
+        return $this->client->post(sprintf('ordermanagement/v1/orders/%s/captures/%s/shipping-info', $id, $captureId), [
+            'json' => $data
+        ]);
+    }
+
+    /**
+     * @param string $id
+     * @param string $captureId
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-trigger-resend-of-customer-communication
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function triggerCaptureSendOut($id, $captureId)
+    {
+        return $this->client->post(sprintf('ordermanagement/v1/orders/%s/captures/%s/trigger-send-out', $id, $captureId));
+    }
+
+    /**
+     * @param string $id
+     * @param array  $data
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-create-a-refund
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function refund($id, array $data)
+    {
+        return $this->client->post(sprintf('ordermanagement/v1/orders/%s/refunds', $id), [
+            'json' => $data
+        ]);
+    }
+
+    /**
+     * @param string $id
+     * @param string $refundId
+     *
+     * @see https://developers.klarna.com/api/#order-management-api-get-refund
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getRefund($id, $refundId)
+    {
+        return $this->client->get(sprintf('ordermanagement/v1/orders/%s/refunds/%s', $id, $refundId));
+    }
+}
