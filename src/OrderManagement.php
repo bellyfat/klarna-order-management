16c99d6c03d6004d9c59897f15b5ae4ddd81e1ef
Jason Grim <jason.grim@klarna.com>
2017-04-27 11:13:42 -0400

    Added license and other misc cleanup

--- src/OrderManagement.php
+++ src/OrderManagement.php
@@ -1,6 +1,32 @@
 <?php namespace KlarnaOrderManagement;
 
-class OrderManagement extends \KlarnaCore\Resource
+/**
+ * Copyright 2017 Jason Grim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package    KlarnaOrderManagement
+ * @author     Jason Grim <me@jasongrim.com>
+ */
+
+use KlarnaCore\Resource;
+
+/**
+ * Class OrderManagement
+ *
+ * @package KlarnaOrderManagement
+ */
+class OrderManagement extends Resource
 {
     /**
      * @param string $id
